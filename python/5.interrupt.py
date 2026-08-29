e4f6788733f53b20ffe3d8de62711cb1538e48bc
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-08-29 16:36:32 +0530

    Agentic workflow which can be used to automate tasks which require pattern recognition

---
+++ python/5.interrupt.py
@@ -0,0 +1,76 @@
+import operator
+from typing import Annotated, List, Literal, TypedDict
+
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph import END, START, StateGraph
+from langgraph.types import Command, interrupt
+
+memory = InMemorySaver()
+config = {"configurable": {"thread_id": "1"}}
+
+
+class State(TypedDict):
+    nlist: Annotated[list["str"], operator.add]
+
+
+def node_a(state: State) -> Command[Literal["b", "c", END]]:
+    print("Entered 'a' node : ")
+    select = state["nlist"][-1]
+    if select == "b":
+        next_node = "b"
+    elif select == "c":
+        next_node = "c"
+    elif select == "q":
+        next_node = END
+    else:
+        admin = interrupt(f"Unexpected input : '{select}'")
+        print(admin)
+        if admin == "continue":
+            next_node = "b"
+        else:
+            next_node = END
+            select = "q"
+    return Command(update=State(nlist=[select]), goto=next_node)
+
+
+def node_b(state: State) -> State:
+    return State(nlist=["B"])
+
+
+def node_c(state: State) -> State:
+    return State(nlist=["C"])
+
+
+builder = StateGraph(State)
+
+# Add nodes
+builder.add_node("a", node_a)
+builder.add_node("b", node_b)
+builder.add_node("c", node_c)
+
+# Add edges
+builder.add_edge(START, "a")
+builder.add_edge("b", END)
+builder.add_edge("c", END)
+
+# compile
+graph = builder.compile(checkpointer=memory)
+
+while True:
+    user = input("b, c, or q to quit : ")
+    input_state = State(nlist=[user])
+    response = graph.invoke(input_state, config)
+
+    if "__interrupt__" in response:
+        print(f"Interrupt : {response['__interrupt__']}")
+        msg = response["__interrupt__"][-1].value
+        print(msg)
+        human = input(f"\n{msg} : ")
+
+        human_response = Command(resume=human)
+        response = graph.invoke(human_response, config)
+
+    if response["nlist"][-1] == "q":
+        print("quit")
+        print(response)
+        break
